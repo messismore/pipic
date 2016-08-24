752e51d7234488caecdadafada38ceba8e06534f
Felix Deiters <username@users.noreply.github.com>
2016-08-24 18:43:32 +0200

    debugging

--- timelapse.py
+++ timelapse.py
@@ -91,7 +91,9 @@ class timelapse:
 
         if self.colourbalance is not 'auto':  # ('497/256', '177/128')
             self.camera.awb_mode = 'off'
-            print type(self.colourbalance)
+            print type(self.colourbalance
+            print self.colourbalance
+            print (Fraction(self.colourbalance[0]), Fraction(self.colourbalance[1]))
             self.camera.awb_gains = (Fraction(self.colourbalance[0]),
                                      Fraction(self.colourbalance[1]))
             print 'WB: ', self.camera.awb_gains
@@ -305,9 +307,8 @@ def main(argv):
     parser.add_argument('-m', '--metering', default='a', type=str, choices=['a','c','l','r'], help='Where to average brightness for brightness calculations.\n"a" measures the whole image, "c" uses a window at the center, "l" meters a strip at the left, "r" uses a strip at the right.' )
     parser.add_argument('-I', '--iso', default=100, type=int, help='Set ISO.' )
     parser.add_argument('-c', '--colourbalance', nargs='2', default='auto',
-                        type=str,
-                        help=('Set white balance as red and blue. '
-                              '''Eg. \'493/256\' '387/256\' ''')
+                        type=str, help='Set white balance as red and blue. '
+                                             '''Eg. \'493/256\' '387/256\' ''')
 
     args=parser.parse_args()
     TL = timelapse(w=args.width, h=args.height, interval=args.interval,
