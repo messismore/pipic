1a88369652ca0fc6b8d2bfb2faba7d43c96a091b
Felix Deiters <username@users.noreply.github.com>
2016-08-24 20:54:46 +0200

    awb_gains test

---
+++ wb_gains_test.py
@@ -0,0 +1,20 @@
+import picamera
+import time
+from fractions import Fraction
+
+
+camera = picamera.PiCamera()
+camera.framerate = 10
+
+
+time.sleep(1)
+# This capture discovers initial AWB and SS.
+camera.capture('try.jpg')
+camera.shutter_speed = camera.exposure_speed
+currentss = camera.exposure_speed
+camera.exposure_mode = 'off'
+camera.resolution = (1920, 1080)
+camera.awb_mode = 'off'
+camera.awb_gains = Fraction(497, 256), Fraction(177, 128)
+
+print camera.awb_gains
