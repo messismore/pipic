7c1e98a389e3e50cebe1036ebf47bfd8aa20b562
Felix Deiters <username@users.noreply.github.com>
2016-08-27 17:10:16 +0200

    MergeHDRStack.py now accepts lists of files of arbitrary length

--- MergeHDRStack.py
+++ MergeHDRStack.py
@@ -7,5 +7,6 @@ def MergeHDRStack(filenames, image_name=None):
     Receives a list of filenames and passes them to enfuse
     in order to create a high dynamic range image
     """
-    call(['enfuse', '--output=%s' % (image_name), filenames[0], filenames[1], filenames[2]])
+    filenames = ', '.join(filenames)
+    call(['enfuse', '--output=%s' % (image_name), filenames])
     return True
